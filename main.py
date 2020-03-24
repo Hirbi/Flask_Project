9ae854ccb38753e8cd5bf46d10864a38358524cd
Romas <roma.sh.14@mail.ru>
2020-03-25 00:04:09 +0300

    Login, Register, add obj without pictures :/

--- main.py
+++ main.py
@@ -1,6 +1,110 @@
+from flask import Flask, render_template, redirect, request, make_response, session, abort, jsonify
+from flask_wtf import FlaskForm
+from flask_login import LoginManager, login_user, logout_user, current_user, login_required
+from wtforms import StringField, PasswordField, SubmitField, TextAreaField, BooleanField, FileField
+from wtforms.validators import DataRequired
 from flask import Flask, render_template
+from data import db_session, objects, users
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    sessions = db_session.create_session()
+    return sessions.query(users.User).get(user_id)
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+class RegisterForm(FlaskForm):
+    email = StringField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    password_again = PasswordField('Повторите пароль', validators=[DataRequired()])
+    name = StringField('Имя пользователя', validators=[DataRequired()])
+    submit = SubmitField('Зарегистрироваться')
+
+
+class LoginForm(FlaskForm):
+    email = StringField("Почта", validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
+
+
+class ObjectsForm(FlaskForm):
+    name = StringField('Название', validators=[DataRequired()])
+    description = StringField('Описание', validators=[DataRequired()])
+    pictures = FileField('??Фото??', validators=[DataRequired()])
+    submit = SubmitField('Сохранить')
+
+
+@app.route('/add_obj', methods=['GET', 'POST'])
+@login_required
+def add_obj():
+    form = ObjectsForm()
+    if form.validate_on_submit():
+        sessions = db_session.create_session()
+        obj = objects.Object()
+        obj.name = form.name.data
+        obj.description = form.description.data
+        obj.pictures = form.pictures.data
+        current_user.news.append(obj)
+        sessions.merge(current_user)
+        sessions.commit()
+        return redirect('/')
+    return render_template('add_objects.html', title='Добавление новости', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        sessions = db_session.create_session()
+        user = sessions.query(users.User).filter(users.User.email == form.email.data).first()
+        if user and user.password == form.password.data:
+            login_user(user, remember=form.remember_me.data)
+            return redirect('/')
+        return render_template('login.html', message='Неправильный логин или пароль', form=form)
+    return render_template('login.html', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect('/')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        sessions = db_session.create_session()
+        if sessions.query(users.User).filter(users.User.email == form.email.data).first():
+            return render_template('register.html',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = users.User(
+            name=form.name.data,
+            email=form.email.data,
+            password=form.password.data
+        )
+        user.set_password(form.password.data)
+        sessions.add(user)
+        sessions.commit()
+        return redirect('/login')
+    return render_template('register.html', form=form)
 
 
 @app.route('/')
@@ -10,4 +114,5 @@ def main_page():
 
 
 if __name__ == '__main__':
+    db_session.global_init("db/blogs.sqlite")
     app.run(port=8080, host='127.0.0.1')
