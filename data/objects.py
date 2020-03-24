a0bfbf951a225e1cc624d698e0f47099d4edb4b9
Hirbi <46817600+Hirbi@users.noreply.github.com>
2020-03-24 10:35:08 +0300

    Update objects.py
    mistake

--- data/objects.py
+++ data/objects.py
@@ -7,7 +7,7 @@ from .db_session import SqlAlchemyBase
 from sqlalchemy_serializer import SerializerMixin
 
 
-class User(SqlAlchemyBase, UserMixin, SerializerMixin):
+class Object(SqlAlchemyBase, UserMixin, SerializerMixin):
     __tablename__ = 'objects'
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
     name = sqlalchemy.Column(sqlalchemy.String, nullable=False)
