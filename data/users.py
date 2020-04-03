6741172fd052ec7462af61cacd02ff0e0e0c256a
Romas <roma.sh.14@mail.ru>
2020-04-03 13:12:39 +0300

    Added not found avatar pic

--- data/users.py
+++ data/users.py
@@ -18,7 +18,7 @@ class User(SqlAlchemyBase, UserMixin, SerializerMixin):
     town = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     phone = sqlalchemy.Column(sqlalchemy.String, nullable=False)
     avatar = sqlalchemy.Column(sqlalchemy.String, nullable=False,
-                               default='/static/img/logo.png')
+                               default='/static/img/n_f_avatar.jpg')
     admin = sqlalchemy.Column(sqlalchemy.Integer, default=0)
     block = sqlalchemy.Column(sqlalchemy.Boolean, default=False)
     objects = orm.relation('Object', back_populates='user')
